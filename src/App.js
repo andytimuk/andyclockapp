38fd07b015634bbff4dac3578f5ca003ccf4ecee
andytimuk <cvonlyaccount@hotmail.co.uk>
2022-10-26 20:24:52 +0100

    andyclockappv1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Clock from './Components/Clock';
+
+function App() {
+  return (
+    <div className="App">
+      <Clock/>
+    </div>
+  );
+}
+
+export default App;
